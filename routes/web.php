798bff4b95c126e76b9e03e705447dbc660d813e
al-kafi-sohag-6 <85807115+al-kafi-sohag-6@users.noreply.github.com>
2022-09-05 23:22:30 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,6 @@ use App\Http\Controllers\UserController;
 
 use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/clear-cache', function(){
     Artisan::call('optimize:clear');
