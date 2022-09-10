a585fbaf8b56b5adbceefbda99fa38e982456d21
Saadsakib <sakib.euitsols@gmail.com>
2022-09-10 20:06:17 +0600

    create-module done

--- resources/views/pages/setup/credit/index.blade.php
+++ resources/views/pages/setup/credit/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Shift Management')
+@section('title', 'Credit Management')
 
 @push('third_party_stylesheets')
     <link href="{{ asset('assets/js/DataTable/datatables.min.css') }}" rel="stylesheet">
