30d730d9cf982b13b69597a638b27a49895573db
Saadsakib <sakib.euitsols@gmail.com>
2022-10-03 16:31:18 +0600

    grade-calculation done

--- app/Http/Controllers/setup/GradeCalculationController.php
+++ app/Http/Controllers/setup/GradeCalculationController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use App\Models\Grade;
 use App\Models\Lettergrade;
+use DataTables;
+use Illuminate\Support\Facades\Auth;
 
 class GradeCalculationController extends Controller
 {
@@ -17,9 +19,37 @@ class GradeCalculationController extends Controller
         return $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
     {
         $this->check_access('view grade-calculation');
+        if ($request->ajax()) {
+            $grades = Grade::with(['created_user', 'letterGrade'])->where('deleted_at', null)->latest()->get();
+            return Datatables::of($grades)
+                    ->addIndexColumn()
+                    ->editColumn('created_at', function($data){ $formatedDate = date('d-m-Y', strtotime($data->created_at)); return $formatedDate; })
+                    ->editColumn('grade_point', function($data){ $format_grade_point = number_format((float)$data->grade_point, 2, '.', ''); return $format_grade_point; })
+                    ->editColumn('marks', function($data){ $format_marks =  $data->mark_start.' - '. $data->mark_end; return $format_marks; })
+                    ->addColumn('created_user', function ($data) {
+                        return $data->created_user->name ?? 'system';
+                    })
+                    ->addColumn('letterGrade', function ($data) {
+                        return $data->letterGrade->name;
+                    })
+                    ->addColumn('action', function($data){
+                        $btn = '<div class="btn-group">';
+                        $btn .= '<a href="javascript:void(0)" class="btn btn-info btnView" data-id="' .$data->id. '"><i class="fas fa-eye"></i></a>';
+                        if(Auth::user()->can('edit grade-calculation') || Auth::user()->role->id == 1){
+                            $btn .= '<a href="'.route("grade.edit", $data->id).'" class="btn btn-dark btnEdit"><i class="fas fa-edit"></i></a>';
+                        }
+                        if(Auth::user()->can('delete grade-calculation') || Auth::user()->role->id == 1){
+                            $btn .= '<a href="'.route("grade.destroy", $data->id).'" class="btn btn-danger btnDelete"><i class="fas fa-trash"></i></a>';
+                        }
+                        $btn .= '</div>';
+                        return $btn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
         $n['db_data'] = Grade::where('deleted_at', null)->latest()->get();
         return view('pages.setup.grade.index',$n);
     }
