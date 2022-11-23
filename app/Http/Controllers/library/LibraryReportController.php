eef54a4ff634a3f8e33e3ced3225126cd52f7db0
al-kafi-sohag-6 <85807115+al-kafi-sohag-6@users.noreply.github.com>
2022-11-23 15:07:54 +0600

    Update LibraryReportController.php

--- app/Http/Controllers/library/LibraryReportController.php
+++ app/Http/Controllers/library/LibraryReportController.php
@@ -48,7 +48,7 @@ class LibraryReportController extends Controller
         $n['returned_info_all'] = $returned->get();
 
        $delay  = AssignBook::where('deleted_by',null)
-                               ->whereBetween('return_date',[$n['str_date'],$n['end_date']])->where('status','-1');
+                               ->whereBetween('return_date',[$n['str_date'],$n['end_date']]);
                                if($n['user_id']){
                                     $delay->where('created_by',$n['user_id']);
                                 }
