3fc24a82df8196a79531f3f0b7607eb0b6fa5ab6
al-kafi-sohag-6 <85807115+al-kafi-sohag-6@users.noreply.github.com>
2022-12-07 02:14:45 +0600

    test trait fix

--- app/Models/AssetBrand.php
+++ app/Models/AssetBrand.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class AssetBrand extends Model implements HasMedia
 {
